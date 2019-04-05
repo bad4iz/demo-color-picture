3d8d763060fc8bd1ff9d7764dd974f2a3f663ece
bad4iz <bad4iz@gmail.com>
2019-04-05 21:35:55 +0400

    sdfas

--- config/dev-server.js
+++ config/dev-server.js
@@ -11,7 +11,7 @@ app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output
 app.use(webpackHotMiddleware(compiler))
 
 app.get("/assets", function(req, res) {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/src/index.html')
 })
 
 
